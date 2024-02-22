f5586b38c8b6b71c3a7f5a530e1799157f449549
Siva <114721846+Siva27012004@users.noreply.github.com>
2024-02-22 21:20:46 +0530

    Add files via upload

---
+++ Tutor/GetStudentData.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EndSem.Tutor
+{
+    public partial class GetStudentData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Form["tutorId"] != null)
+            {
+                int tutorId = Convert.ToInt32(Request.Form["tutorId"]);
+                // Fetch student data for the selected tutor from the database
+                DataTable dt = FetchStudentDataByTutor(tutorId);
+
+                // Render the student data as HTML and send it as the response
+                Response.Clear();
+                Response.Write(RenderStudentDataAsHtml(dt));
+                Response.End();
+            }
+        }
+
+        private string RenderStudentDataAsHtml(DataTable dt)
+        {
+            throw new NotImplementedException();
+        }
+
+        private DataTable FetchStudentDataByTutor(int tutorId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tutor/TutorHome.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Tutor/TutorMst.Master" AutoEventWireup="true" CodeBehind="TutorHome.aspx.cs" Inherits="EndSem.Tutor.TutorHome" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div style="background-image:url('../Image/EndSem1.jpeg'); width:100%; height:720px; background-repeat:no-repeat;background-size:cover;background-attachment:fixed;">
+        <div class ="container p-md-4 p-sm-4">
+            <div>
+                <asp:Label ID="lblMsg" runat="server"></asp:Label>
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
+
+
+
+
+
+
+
